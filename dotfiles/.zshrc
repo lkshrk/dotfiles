2657da17e6c85cc80c5c41de61302bb7d4b09b3b
Lukas Harke <lukas@harke.me>
2021-05-21 15:09:20 +0200

    initial

---
+++ dotfiles/.zshrc
@@ -0,0 +1,27 @@
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/lkshrk/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="robbyrussell"
+
+
+plugins=(
+    cp
+    extract
+    git
+    history
+    jsontools
+    vscode
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)
+
+LANG=en_US.utf8
+
+source $ZSH/oh-my-zsh.sh
---
+++ setup.sh
@@ -0,0 +1,32 @@
+xcode-select --install
+softwareupdate --install-rosetta
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew update
+
+### Tools and Software
+brew install golang ffmpeg lame tree vim wget youtube-dl tmux mas python
+brew install bartender istat-menus iina little-snitch the-unarchiver alt-tab \
+visual-studio-code iterm2 \
+discord telegram whatsapp \
+steelseries-engine steelseries-exactmouse-tool logitech-camera-settings logitech-g-hub
+
+sh -c "RUNZSH=no; $(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+
+# dotfiles for zsh
+cp -f dotfiles/.zshrc $HOME/.zshrc
+mkdir -p $HOME/.dotfiles
+cp dotfiles/* $HOME/.dotfiles/
+
+### osx configs
+chflags nohidden ~/Library #Show Library
+defaults write com.apple.finder AppleShowAllFiles YES #Show Hidden Files
+defaults write com.apple.finder ShowPathbar -bool true #Show Path Bar
+defaults write com.apple.finder ShowStatusBar -bool true #Show status Bar
