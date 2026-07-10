728d2c09f374be2eaf08a1422eec0d36737296ee
lkshrk <dev@harke.me>
2026-07-10 04:37:05 +0200

    feat(nvim): bind ctrl+shift+f to live_grep, enable tmux extended-keys
    Requires CSI-u capable terminal (ghostty); extended-keys on lets tmux
    pass the distinct keycode through instead of degrading it to ctrl+f.

--- dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -206,6 +206,8 @@ return {
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    -- Needs CSI-u/kitty keyboard protocol (ghostty; tmux with extended-keys on).
+    vim.keymap.set('n', '<C-S-f>', builtin.live_grep, { desc = 'Search file contents' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sc', builtin.git_status, { desc = '[S]earch Git [C]hanges' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
