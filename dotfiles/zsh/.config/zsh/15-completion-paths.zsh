378b8d8f794734017989c2ee21082e5d59731c94
lkshrk <dev@harke.me>
2026-07-18 13:26:10 +0200

    kk

--- dotfiles/zsh/.config/zsh/15-completion-paths.zsh
+++ dotfiles/zsh/.config/zsh/15-completion-paths.zsh
@@ -1,3 +1,4 @@
+[[ -d /opt/homebrew/share/zsh/site-functions ]] && fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
 [[ -d "$HOME/.docker/completions" ]] && fpath=("$HOME/.docker/completions" $fpath)
 [[ -d "$HOME/.cache/zsh/completions" ]] && fpath=("$HOME/.cache/zsh/completions" $fpath)
 [[ -d "$HOME/.config/zsh/completions" ]] && fpath=("$HOME/.config/zsh/completions" $fpath)
