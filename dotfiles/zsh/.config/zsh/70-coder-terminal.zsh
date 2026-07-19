d35b253942120e931713fc050ecd12e7ac9b065d
lkshrk <dev@harke.me>
2026-07-19 10:31:39 +0200

    fix certs

--- dotfiles/zsh/.config/zsh/70-coder-terminal.zsh
+++ dotfiles/zsh/.config/zsh/70-coder-terminal.zsh
@@ -11,6 +11,6 @@ if [[ -n ${CODER_WORKSPACE_NAME:-} ]]; then
   _coder_clear_mouse_modes
 
   if [[ -z ${TMUX:-} ]] && command -v tmux >/dev/null 2>&1; then
-    exec tmux new-session -A -s default
+    exec tmux new-session -A -D -s default
   fi
 fi
